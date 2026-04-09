fcf20ea39971e5da1cd0f64ae4813184984d705e
Ashutosh5220 <165662915+Ashutosh5220@users.noreply.github.com>
2026-04-10 00:17:08 +0530

    fixed db using mysql_url

--- db.js
+++ db.js
@@ -1,18 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
     console.log("DB connection failed ❌", err);
   } else {
-    console.log("Connected to Railway DB ✅");
+    console.log("Connected to DB ✅");
   }
 });
 
