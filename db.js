a5f00b0364b09f7dec49769fb5341cd02c044c12
Ashutosh5220 <165662915+Ashutosh5220@users.noreply.github.com>
2026-04-10 10:43:46 +0530

    Updated DB config and added Postman collection

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const connection = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
     console.log("DB connection failed ❌", err);
   } else {
@@ -16,4 +11,4 @@ db.connect((err) => {
   }
 });
 
-module.exports = db;
+module.exports = connection;
